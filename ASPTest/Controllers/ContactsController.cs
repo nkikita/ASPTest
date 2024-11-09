fa15516411f7eacc3a0228e9267685d3af182ef4
Nikita <nikita.korolev.14.08.2005@mail.ru>
2024-11-09 14:01:56 +0500

    Добавьте файлы проекта.

---
+++ ASPTest/Controllers/ContactsController.cs
@@ -0,0 +1,30 @@
+﻿using ASPTest.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Xml.Linq;
+
+namespace ASPTest.Controllers
+{
+    public class ContactsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Check(Contact cont) 
+        {
+            if(ModelState.IsValid)
+            {
+                string textToWrite = $"Name: {cont.Name}, Email: {cont.Email}, Age: {cont.Age}, Message: {cont.Mess}\n";
+
+                await System.IO.File.AppendAllTextAsync("C:/Users/nikit/OneDrive/Рабочий стол/codes/vs/ASPTest/ASPTest/dbimitation.txt", textToWrite);
+
+                return View("ContTHX");
+
+            }
+            return View("Index");
+        }
+    }
+}
---
+++ ASPTest/Models/Contact.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASPTest.Models
+{
+    public class Contact
+    {
+        [Display(Name = "Имя")]
+        [Required(ErrorMessage ="NOOOOOOOOOOOOOOOOOOOOOOI")]
+        public string Name { get; set; }
+
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "NOOOOOOOOOOOOOOOOOOOOOOI")]
+        public string Email { get; set; }
+
+        [Display(Name = "Возраст")]
+        [Required(ErrorMessage = "NOOOOOOOOOOOOOOOOOOOOOOI")]
+        public int Age{ get; set; }
+
+        [Display(Name = "Сообщение")]
+        [Required(ErrorMessage = "NOOOOOOOOOOOOOOOOOOOOOOI")]
+        public string Mess { get; set; }
+    }
+}
